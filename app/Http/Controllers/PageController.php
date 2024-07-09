b5c0e4844cd3ca9c25e06df807dd8ea8f77a6217
Valeria Buonpane <valeriabuonpane@gmail.com>
2024-07-09 21:26:42 +0200

    aggiustato errori su browser e proseguito con controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,17 +12,40 @@ class PageController extends Controller
         $titleIsvibible = false;
 
         $articles = [
-            ['title' => 'Titolo articolo #1', 'categoria' => 'CATEGORIA: loft', 'description' => 'esempio description 1'],
-            ['title' => 'Titolo articolo #2', 'categoria' => 'CATEGORIA: ville al mare', 'description' => 'esempio description 2'],
-            ['title' => 'Titolo articolo #3', 'categoria' => 'CATEGORIA: ville in montagna', 'description' => 'esempio description 3'],
+            ['title' => 'Titolo articolo #1', 'category' => 'CATEGORIA: loft', 'description' => 'esempio description 1', 'visible' => true],
+            ['title' => 'Titolo articolo #2', 'category' => 'CATEGORIA: ville al mare', 'description' => 'esempio description 2', 'visible' => true],
+            ['title' => 'Titolo articolo #3', 'category' => 'CATEGORIA: ville in montagna', 'description' => 'esempio description 3', 'visible' => true],
         ];
 
 
         return view ('articles', [
-            'title'=>'Articoli - Presto.it',
-            'titleh1'=>'Articoli',
-            'description'=> 'Qui si inserisce il testo che vogliamo aggiungere',
-            'articles'=> $articles,
+            'title' => 'Articoli - Presto.it',
+            'titleh1' => 'Articoli',
+            'description' => 'Qui si inserisce il testo che vogliamo aggiungere',
+            'articles' => $articles,
+            'titleIsVisible' => $titleIsvibible,
         ]);
     }
+
+    public function article($id){
+        function ($id = null) {
+	
+            $articles = [
+                ['title' => 'Titolo articolo #1', 'categoria' => 'CATEGORIA: loft', 'description' => 'esempio description 1'],
+                ['title' => 'Titolo articolo #2', 'categoria' => 'CATEGORIA: ville al mare', 'description' => 'esempio description 2'],
+                ['title' => 'Titolo articolo #3', 'categoria' => 'CATEGORIA: ville in montagna', 'description' => 'esempio description 3'],
+            ];
+        
+            if(!array_key_exists($id, $articles)) {
+                abort(404); //questa funzione restituisce una pg errore 404
+            };
+        
+            
+            $articles = $articles[$id];
+        
+            return view('article',['article'=> $article]);
+           
+        };
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,7 @@ Route::get('/', function () {
 Route::get('/articoli', [App\Http\Controllers\PageController::class, 'articles'])->name('articles'); 
 
 
-Route::get('/articoli/{id}', function ($id = null) {
-    
-	$articles = [
-		['title' => 'Titolo articolo #1', 'categoria' => 'CATEGORIA: loft', 'description' => 'esempio description 1'],
-		['title' => 'Titolo articolo #2', 'categoria' => 'CATEGORIA: ville al mare', 'description' => 'esempio description 2'],
-		['title' => 'Titolo articolo #3', 'categoria' => 'CATEGORIA: ville in montagna', 'description' => 'esempio description 3'],
-	];
-
-	if(!array_key_exists($id, $articles)) {
-		abort(404); //questa funzione restituisce una pg errore 404
-	}
-
-	
-	$articles = $articles[$id];
-
-	return view('article',['article'=> $article]);
-   
-})->name('articles.show');
+Route::get('/articoli/{id}', [App\Http\Controllers\PageController::class, 'article' ])->name('articles.show');
 
 
 
